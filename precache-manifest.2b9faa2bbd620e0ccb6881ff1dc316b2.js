0d1da8df18d0284a2393cfa046bed00da2f9725b
lepis <leooblivion24@gmail.com>
2020-08-03 13:44:22 +0700

    Updates

---
+++ precache-manifest.2b9faa2bbd620e0ccb6881ff1dc316b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c30ffe8319b13bc01d238824e49572c",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "e5f371f9ca0b299f4c64",
+    "url": "/shopping-cart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d402366d6a8e1f9efa86",
+    "url": "/shopping-cart/static/css/main.0b5dbbbf.chunk.css"
+  },
+  {
+    "revision": "e5f371f9ca0b299f4c64",
+    "url": "/shopping-cart/static/js/2.ff7e6ed1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-cart/static/js/2.ff7e6ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d402366d6a8e1f9efa86",
+    "url": "/shopping-cart/static/js/main.e63fbd74.chunk.js"
+  },
+  {
+    "revision": "7fdd6229da8042d146b0",
+    "url": "/shopping-cart/static/js/runtime-main.9f3311bc.js"
+  },
+  {
+    "revision": "9f0b9e8337079f4e30256664ac722f82",
+    "url": "/shopping-cart/static/media/exclusive.9f0b9e83.png"
+  },
+  {
+    "revision": "09f6960577c34835e56413249edfa87d",
+    "url": "/shopping-cart/static/media/image1.09f69605.png"
+  },
+  {
+    "revision": "8442d16d036356b52333b968182e1376",
+    "url": "/shopping-cart/static/media/user-1.8442d16d.png"
+  },
+  {
+    "revision": "ec7c8490818b6dc8f57c63b8067717b3",
+    "url": "/shopping-cart/static/media/user-2.ec7c8490.png"
+  },
+  {
+    "revision": "4ccad0abf14781d68ebb337509c71abd",
+    "url": "/shopping-cart/static/media/user-3.4ccad0ab.png"
+  }
+]);
